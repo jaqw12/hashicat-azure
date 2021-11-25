d448d7fa1b3b314dfaa6a27d278e52fce0e7b174
AJain <amrishajain@gmail.com>
2021-11-25 13:12:55 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,8 +2,5 @@ module "network" {
   source  = "app.terraform.io/instruqttest/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  variable "resource_group_name" {
-  description = "The region where the virtual network is created."
-  default     = "test"
-}
+  resource_group_name = "test"
 }
